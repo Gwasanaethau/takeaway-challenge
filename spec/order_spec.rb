13d7e12ea27b8b1abb8597aa6c1a0f0031aef5dd
Mark Pokorny <iridium193@gmail.com>
2015-04-23 12:55:17 +0100

    Minor edits.

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,11 +1,6 @@
 require 'order'
-# require 'customer'
 
 describe Order do
-  # let!(:subject) { described_class.new(double :customer) }
-
-  # it { is_expected.to respond_to(:sum_total) }
-
   it 'sums up to €5 when a hamburger is chosen' do
     menu = double(:menu, dishes: { 'hamburger' => 5, 'cheeseburger' => 5.5 })
     customer = double(:customer, menu: menu, choices: { 'hamburger' => 1 })
@@ -20,9 +15,10 @@ describe Order do
     expect(order.sum_total).to eq 5.5
   end
 
-  it 'sums up to €10.50 when a cheeseburger is chosen' do
+  it 'sums up to €10.50 when a hamburger and a cheeseburger is chosen' do
     menu = double(:menu, dishes: { 'hamburger' => 5, 'cheeseburger' => 5.5 })
-    customer = double(:customer, menu: menu, choices: { 'cheeseburger' => 1, 'hamburger' => 1 })
+    customer = double(:customer, menu: menu, choices: { 'cheeseburger' => 1,
+                                                        'hamburger' => 1 })
     order = described_class.new(customer)
     expect(order.sum_total).to eq 10.5
   end
